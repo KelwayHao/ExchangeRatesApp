fb34983c3e49e165dd1ba889f4ad2ed9165b1a26
KelwayHao <kelwaycf@gmail.com>
2022-07-16 01:53:20 +0300

    Add PopupMenu

--- app/src/main/java/com/kelway/exchangeratesapp/presentation/MainActivity.kt
+++ app/src/main/java/com/kelway/exchangeratesapp/presentation/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.kelway.exchangeratesapp.presentation
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import android.widget.PopupMenu
 import androidx.appcompat.app.AppCompatActivity
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.kelway.exchangeratesapp.R
@@ -17,21 +20,57 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         ExchangeRatesApplication.appComponent?.inject(this)
-        openFragment(R.id.frame, PopularFragment.newInstance(), PopularFragment.TAG)
-    }
-
-    override fun onStart() {
-        super.onStart()
+        openPopularFragment()
         initView()
     }
 
     private fun initView() {
+        binding.sortImageButton.setOnClickListener { view: View ->
+            showSortMenu(view, R.menu.sort_menu)
+        }
         binding.popularButton.setOnClickListener {
-            openFragment(R.id.frame, PopularFragment.newInstance(), PopularFragment.TAG)
+            openPopularFragment()
         }
-
         binding.favoritesButton.setOnClickListener {
-            openFragment(R.id.frame, FavoritesFragment.newInstance(), FavoritesFragment.TAG)
+            openFavoritesFragment()
         }
+    }
+
+    private fun showSortMenu(view: View, menuRes: Int) {
+        val popupMenu = PopupMenu(this, view)
+        popupMenu.menuInflater.inflate(menuRes, popupMenu.menu)
+        popupMenu.setOnMenuItemClickListener { menuItem: MenuItem ->
+            when (menuItem.itemId) {
+                R.id.sortByAlphabet -> {
+
+                }
+                R.id.sortByAlphabetReverse -> {
+
+                }
+                R.id.sortByPriceLowest -> {
+
+                }
+                R.id.sortByPriceHighest -> {
+
+                }
+
+            }
+            return@setOnMenuItemClickListener true
+        }
+        popupMenu.show()
+
+        val popup = PopupMenu::class.java.getDeclaredField("mPopup")
+        popup.isAccessible = true
+        val menu = popup.get(popupMenu)
+        menu.javaClass.getDeclaredMethod("setForceShowIcon",Boolean::class.java)
+            .invoke(menu,true)
+    }
+
+    private fun openPopularFragment() {
+        openFragment(R.id.frame, PopularFragment.newInstance(), PopularFragment.TAG)
+    }
+
+    private fun openFavoritesFragment() {
+        openFragment(R.id.frame, FavoritesFragment.newInstance(), FavoritesFragment.TAG)
     }
 }
