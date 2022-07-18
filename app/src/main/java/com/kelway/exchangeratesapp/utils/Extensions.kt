17f9858e4380e9c16b922ea8278caf1db1e98732
KelwayHao <kelwaycf@gmail.com>
2022-07-19 00:21:22 +0300

    start solve task

--- app/src/main/java/com/kelway/exchangeratesapp/utils/Extensions.kt
+++ app/src/main/java/com/kelway/exchangeratesapp/utils/Extensions.kt
@@ -2,6 +2,8 @@ package com.kelway.exchangeratesapp.utils
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
+import com.kelway.exchangeratesapp.domain.model.Currency
+import com.kelway.exchangeratesapp.domain.model.CurrencyItem
 
 fun FragmentActivity.openFragment(idContainer: Int, fragment: Fragment, tag: String) {
     supportFragmentManager.beginTransaction()
